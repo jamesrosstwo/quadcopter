5371f5c91262cc9ce67468ba1fa9ad650c2e8cd6
James <james.rosstwo@gmail.com>
2019-01-27 14:41:15 -0500

    add update timer

--- main.py
+++ main.py
@@ -2,20 +2,50 @@ import json
 from text_to_speech import Speech
 from speech_to_text import Recognizer
 from watson import Watson
+from server import Server
+import datetime
 
-if __name__ == "__main__":
+
+def load_config():
     with open('config.json') as f:
-        config = json.load(f)
+        return json.load(f)
 
+
+def init_services():
+    global assistant, tts, recognizer, web_server
     print("Initializing conversation...")
     assistant = Watson()
     print("Initializing voice...")
     tts = Speech()
-    print("Initializing speech recognition")
+    print("Initializing speech recognition...")
     recognizer = Recognizer()
 
+    # maybe just store this in memory?
+    with open('config.json') as f:
+        data = json.load(f)
+        print("Initializing web server...")
+        web_server = Server(data)
+
+
+def update_data(update, delay_ms):
+    td = (update - datetime.datetime.now()).total_seconds() * 1000
+    if td < 0:
+        return datetime.datetime.now() + datetime.timedelta(milliseconds=delay_ms)
+    return update
+
+
+
+if __name__ == "__main__":
+    data_cooldown = 500
+    next_update = datetime.datetime.min
+    config = load_config()
+    init_services()
+
     print("Watson:", assistant.message(""))
-    for i in range(20):
-        message = assistant.message(recognizer.listen())
-        print("Watson:", message)
-        tts.say(message)
+    while True:
+        next_update = update_data(next_update, data_cooldown)
+
+    # if buttonPressed:
+    # message = assistant.message(recognizer.listen())
+    # print("Watson:", message)
+    # tts.say(message)
--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 from bottle import route, run, template
 
 
-@route('/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+class Server:
+
+    def __init__(self, data):
+        self.template = data
+
+    @route('/')
+    def index(self):
+        return template(self.template)
+
+
+    def start(self):
+        run(host='0.0.0.0', port=8080)
 
 
 if __name__ == "__main__":
